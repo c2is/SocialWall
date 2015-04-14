f8c0f4ed700ea4e8f26f4324e90f75535998b89e
Guillaume Manen <g.manen@c2is.fr>
2015-04-14 18:20:37 +0200

    Error management

--- src/GooglePlus/GooglePlusManager.php
+++ src/GooglePlus/GooglePlusManager.php
@@ -52,7 +52,12 @@ class GooglePlusManager extends AbstractSocialNetwork
     public function getResult(array $params = array(), array $queryParams = array())
     {
         $service = new \Google_Service_Plus($this->client);
-        $results = $service->activities->listActivities(sprintf('+%s', $params['user_id']), 'public', $queryParams);
+
+        try {
+            $results = $service->activities->listActivities(sprintf('+%s', $params['user_id']), 'public', $queryParams);
+        } catch (\Exception $e) {
+            error_log(sprintf('Error calling Google Plus API : %s', $e->getMessage()));
+        }
 
         $socialItems = array();
 
