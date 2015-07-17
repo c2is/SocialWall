536ab66ff8ad2194b198ca1c416b7f56e965404e
Guillaume Manen <g.manen@c2is.fr>
2015-07-17 11:30:20 +0200

    Refactoring

--- src/GooglePlus/Model/SocialUser.php
+++ src/GooglePlus/Model/SocialUser.php
@@ -3,8 +3,6 @@
 namespace C2iS\SocialWall\GooglePlus\Model;
 
 use C2iS\SocialWall\Google\Model\AbstractSocialUser;
-use C2iS\SocialWall\Model\SocialUserInterface;
-use JMS\Serializer\Annotation as Serializer;
 
 /**
  * Class SocialUser
