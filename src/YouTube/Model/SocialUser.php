077c3f0c6b0377195efe3641da52f471e1e02e3c
Guillaume Manen <g.manen@c2is.fr>
2015-08-12 11:16:58 +0200

    Refactoring

--- src/YouTube/Model/SocialUser.php
+++ src/YouTube/Model/SocialUser.php
@@ -3,8 +3,6 @@
 namespace C2iS\SocialWall\YouTube\Model;
 
 use C2iS\SocialWall\Google\Model\AbstractSocialUser;
-use C2iS\SocialWall\Model\SocialUserInterface;
-use JMS\Serializer\Annotation as Serializer;
 
 /**
  * Class SocialUser
